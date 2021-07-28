b4aef7551023f4aea29e83182bf49f53e8738e73
shrestsav <shrestsav@gmail.com>
2021-07-28 21:48:47 +0545

    tabwidth set to 2

--- laravel/prettier.config.js
+++ laravel/prettier.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    trailingComma: 'es5',
-    singleQuote: true,
-    semi: false,
-    htmlWhitespaceSensitivity: 'ignore',
+  trailingComma: 'es5',
+  singleQuote: true,
+  tabWidth: 2,
+  semi: false,
+  htmlWhitespaceSensitivity: 'ignore',
 }
